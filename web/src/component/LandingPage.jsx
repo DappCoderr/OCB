f2df447f99f99433e4954e78301c9873f1bf202b
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-02-13 23:32:08 +0530

    Modify: landing page

--- web/src/component/LandingPage.jsx
+++ web/src/component/LandingPage.jsx
@@ -1,16 +1,26 @@
 import React from "react";
+import bag from "../assets/bag.svg";
+import "./Landing.css";
 
 const LandingPage = () => {
   return (
-    <div>
+    <div className="landing_page">
       <header>
-        <div className="logo">Bag_</div>
+        <div className="logo">bag_</div>
       </header>
       <main>
-        <div className="wrapper">
-          <div>Bag NFT 1</div>
-          <div>Bag NFT 2</div>
-          <div>Bag NFT 3</div>
+        <div className="content">
+          <p>
+            bag is a first every randomize gear box NFT completely on-chian and
+            also a first every NFT project using Flow VRF. There are total 4444
+            nft for sale.
+          </p>
+          <p>Launching Soon....</p>
+        </div>
+        <div className="image_wrapper">
+          <img src={bag} />
+          <img src={bag} />
+          <img src={bag} />
         </div>
       </main>
       <footer>
