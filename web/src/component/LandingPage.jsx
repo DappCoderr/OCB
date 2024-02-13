603756f54641b54a8d4ef4a387c4d44065959012
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-02-13 22:11:25 +0530

    Add: landing page component

---
+++ web/src/component/LandingPage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <header>
+        <div className="logo">Bag_</div>
+      </header>
+      <main>
+        <div className="wrapper">
+          <div>Bag NFT 1</div>
+          <div>Bag NFT 2</div>
+          <div>Bag NFT 3</div>
+        </div>
+      </main>
+      <footer>
+        <section>
+          <div>Build on Flow</div>
+          <div>Flow Logo</div>
+        </section>
+      </footer>
+    </div>
+  );
+};
+
+export default LandingPage;
