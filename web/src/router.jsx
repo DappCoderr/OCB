1e3f91566644c1426c8fcea09bfc023fdeffdd3b
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-05-30 10:01:27 +0530

    Add: router

---
+++ web/src/router.jsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import Home from "./component/Home";
+import Mint from "./component/Mint";
+import FAQ from "./component/FAQ";
+import ErrorBoundary from "./component/ErrorBoundary";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorBoundary />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/mint",
+        element: <Mint />,
+      },
+      {
+        path: "/faq",
+        element: <FAQ />,
+      },
+    ],
+  },
+]);
+
+export default router;
