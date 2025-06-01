7cbefb3ef11d11c07cc30255cbe4785fb461c18c
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-06-01 10:57:58 +0530

    Add: Constants

---
+++ web/src/utils/constants.js
@@ -0,0 +1,4 @@
+export const CONTRACT_ADDRESS = "0xcff932efdb9a69c3";
+export const NODE_ID =
+  "4afbd63916c3cec5bb75a34d607bcec3c670de952ed1556fa103c680b59b6215";
+export const NODE_TYPE = "Consensus";
