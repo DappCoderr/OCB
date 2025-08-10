40985c38f772177bd925652bf14746c5ba58023f
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-08-10 17:00:31 +0530

    setup outlet

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,44 +1,11 @@
-import { useState } from "react";
+import { Outlet } from "react-router-dom";
 import Header from "./component/Header";
-import Home from "./component/Home";
-import Mint from "./component/Mint";
-import FAQ from "./component/FAQ";
 
 const App = () => {
-  const [walletConnected, setWalletConnected] = useState(false);
-  const [totalMinted, setTotalMinted] = useState(2847);
-  const [currentPage, setCurrentPage] = useState("home");
-
-  const connectWallet = () => {
-    setWalletConnected(!walletConnected);
-  };
-
-  const renderCurrentPage = () => {
-    switch (currentPage) {
-      case "mint":
-        return (
-          <Mint
-            walletConnected={walletConnected}
-            totalMinted={totalMinted}
-            setTotalMinted={setTotalMinted}
-          />
-        );
-      case "faq":
-        return <FAQ />;
-      default:
-        return <Home setCurrentPage={setCurrentPage} />;
-    }
-  };
   return (
     <div className="min-h-screen bg-gray-50 font-mono text-sm leading-relaxed">
-      <Header
-        walletConnected={walletConnected}
-        connectWallet={connectWallet}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
-
-      {renderCurrentPage()}
+      <Header />
+      <Outlet />
     </div>
   );
 };
