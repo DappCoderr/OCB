ca0aae90cc1b06f6ff0619d4e278a5901924fab1
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-09-07 22:27:41 +0530

    setup landing page

---
+++ web/src/pages/landing.jsx
@@ -0,0 +1,19 @@
+import HeroSection from '../component/section/HeroSection';
+import FAQSection from '../component/section/FAQSection';
+import BenefitsSection from '../component/section/BenefitsSection';
+import WhoIsOCBFor from '../component/section/WhoIsOCBFor';
+import HowItWorksSection from '../component/section/HowItWorksSection';
+
+const Landing = () => {
+  return (
+    <main className="min-h-screen bg-gradient-to-b from-gray-50 to-white">
+      <HeroSection />
+      <BenefitsSection />
+      <WhoIsOCBFor />
+      <HowItWorksSection />
+      <FAQSection />
+    </main>
+  );
+}
+
+export default Landing
