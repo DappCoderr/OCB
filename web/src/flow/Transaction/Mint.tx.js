bb9162e1b5ad3226995a787d8e9ccc48db2f259b
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-09-12 21:13:13 +0530

    update: mint bag with fcl

--- web/src/flow/Transaction/Mint.tx.js
+++ web/src/flow/Transaction/Mint.tx.js
@@ -1,40 +1,73 @@
 import * as fcl from '@onflow/fcl';
 
-const MINT = `import Bag from 0xdb2133aaf990813c
+export const MINT = `
+import Bag from 0x337e140cac71c1f0
 import FungibleToken from 0x9a0766d93b6608b7
 import FlowToken from 0x7e60df042a9c0868
 
-transaction(amount:UFix64){
-    let Payment: @FlowToken.Vault
+transaction(user: Address, amount: UFix64, mintQty: UInt64) {
+
+    let flowTokenVault: @FlowToken.Vault
     let collectionRef: &Bag.Collection
-    let userAddr: Address
+    let userVaultRef: &FlowToken.Vault
 
     prepare(signer: auth(Storage, Capabilities) &Account) {
 
         if signer.storage.borrow<&Bag.Collection>(from: Bag.CollectionStoragePath) == nil {
-            signer.storage.save(<- Bag.createEmptyCollection(nftType: Type<@Bag.NFT>()), to: Bag.CollectionStoragePath)
+            signer.storage.save(
+                <- Bag.createEmptyCollection(nftType: Type<@Bag.NFT>()),
+                to: Bag.CollectionStoragePath
+            )
+
             let collectionCap = signer.capabilities.storage.issue<&Bag.Collection>(Bag.CollectionStoragePath)
             signer.capabilities.publish(collectionCap, at: Bag.CollectionPublicPath) 
         }
 
-        self.collectionRef = signer.storage.borrow<&Bag.Collection>(from: Bag.CollectionStoragePath) ?? panic("Could not found the collection")
+        // Borrow reference to the collection
+        self.collectionRef = signer.storage.borrow<&Bag.Collection>(from: Bag.CollectionStoragePath)
+            ?? panic("Could not borrow a reference to a Bag Collection in the user's account")
 
-        let flowVault = signer.storage.borrow<auth(FungibleToken.Withdraw) &FlowToken.Vault>(from: /storage/flowTokenVault)!
-        self.Payment <- flowVault.withdraw(amount: amount) as! @FlowToken.Vault
-        self.userAddr = signer.address
+        // Borrow withdraw capability for FlowToken Vault
+        let vaultRef = signer.storage
+            .borrow<auth(FungibleToken.Withdraw) &FlowToken.Vault>(from: /storage/flowTokenVault)
+            ?? panic("Could not borrow reference to FlowToken Vault")
+
+        // Withdraw total amount user sent for this minting
+        self.flowTokenVault <- vaultRef.withdraw(amount: amount) as! @FlowToken.Vault
+
+        // Borrow deposit reference so we can refund later in execute
+        self.userVaultRef = signer.storage
+            .borrow<&FlowToken.Vault>(from: /storage/flowTokenVault)
+            ?? panic("Could not borrow deposit reference to FlowToken Vault for refund")
     }
 
-    execute{
-        let nft <- Bag.mintNFT(addr: self.userAddr, payment: <- self.Payment)
-        self.collectionRef.deposit(token: <- nft)
-    }
-} `;
+    execute {
+        var i: UInt64 = 0
+        let pricePerMint = Bag.mintPrice
 
-export async function mintNFT(amount) {
+        while i < mintQty {
+            let payment <- self.flowTokenVault.withdraw(amount: pricePerMint) as! @FlowToken.Vault
+            let nft <- Bag.mintNFT(user: user, payment: <- payment)
+            self.collectionRef.deposit(token: <- nft)
+            i = i + 1
+        }
+
+        // Return any leftover FLOW tokens to the user's vault
+        if self.flowTokenVault.balance > 0.0 {
+            self.userVaultRef.deposit(from: <- self.flowTokenVault)
+        } else {
+            destroy self.flowTokenVault
+        }
+    }
+}
+
+`;
+
+export async function mintNFT(user, amount, mintQty) {
   try {
     const response = await fcl.mutate({
       cadence: MINT,
-      args: (arg, t) => [arg(amount, t.UFix64)],
+      args: (arg, t) => [arg(user, t.Address), arg(amount, t.UFix64), arg(mintQty, t.UInt64)],
       payer: fcl.authz,
       proposer: fcl.authz,
       authorizations: [fcl.authz],
