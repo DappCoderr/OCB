fe9c6864cfe33aaded86a941bce2035e79caf35e
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-09-15 22:13:31 +0530

    fix: router

--- web/src/router/AppRouter.jsx
+++ web/src/router/AppRouter.jsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Mint from '../pages/mint';
 import FAQ from '../component/FAQ';
-import Lottery from '../pages/lottery';
 import History from '../pages/history';
 
 const AppRouter = () => (
   <Routes>
     <Route path="/" element={<Mint />} />
-    <Route path="/lottery" element={<Lottery />} />
     <Route path="/history" element={<History />} />
     <Route path="/faq" element={<FAQ />} />
   </Routes>
